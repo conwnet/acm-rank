eedb8f9eaa20d0bd5ea34b7299ac0ba710801e9f
netcon <netcon@live.com>
2016-09-28 21:49:17 +0800

    fix bugs

--- backup.py
+++ backup.py
@@ -4,4 +4,4 @@ db = sqlite3.connect('database.db')
 cur = db.execute('select `id`, `name`, `email` from `acmers`')
 acmers = cur.fetchall()
 for acmer in acmers:
-    print("insert into `acmers` (`id`, `name`, `email`) values ('%s', '%s', '%s');" % (acmer[0], acmer[1], acmer[2]))
+    print("insert into `acmers` (`id`, `name`, `email`, `status`) values ('%s', '%s', '%s', 1);" % (acmer[0], acmer[1], acmer[2]))
